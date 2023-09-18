c06ca17ef3ee46eb5b1c6956edbd1fd2e2f15bb1
TobyJWalker <tobyjwalker03@outlook.com>
2023-09-18 14:04:59 +0100

    Added first test for reading_time

--- tests/test_reading_time.py
+++ tests/test_reading_time.py
@@ -1,2 +1,11 @@
 import pytest
 from lib.reading_time import *
+
+with open("100_word_text.txt", "r") as f:
+    hundred_word_text = f.read()
+
+with open("200_word_text.txt", "r") as f:
+    two_hundred_word_text = f.read()
+
+def test_reading_time_two_hundred_words():
+    assert estimate_reading_time(two_hundred_word_text) == 1
