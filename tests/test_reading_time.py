afc33ade190dd7c89bc162bc86ac78792f33ca07
TobyJWalker <tobyjwalker03@outlook.com>
2023-09-18 14:12:18 +0100

    Added tests for non string inputs to reading_time

--- tests/test_reading_time.py
+++ tests/test_reading_time.py
@@ -15,3 +15,21 @@ def test_reading_time_hundred_words():
 
 def test_reading_time_empty():
     assert estimate_reading_time("") == 0
+
+def test_reading_time_spaces():
+    assert estimate_reading_time("   ") == 0
+
+def test_reading_time_number():
+    with pytest.raises(TypeError) as e:
+        estimate_reading_time(1)
+    return str(e.value) == "Argument must be a string"
+
+def test_reading_time_none():
+    with pytest.raises(TypeError) as e:
+        estimate_reading_time(None)
+    return str(e.value) == "Argument must be a string"
+
+def test_reading_time_list():
+    with pytest.raises(TypeError) as e:
+        estimate_reading_time(['this', 'is', 'a', 'list'])
+    return str(e.value) == "Argument must be a string"
