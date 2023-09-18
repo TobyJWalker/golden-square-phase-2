45ad52fe627d45f883f5ee9f9cac54c16f156b86
TobyJWalker <tobyjwalker03@outlook.com>
2023-09-18 12:19:00 +0100

    Created challenge 1 files and base test

---
+++ tests/test_challenge_1.py
@@ -0,0 +1,8 @@
+from lib.challenge_1 import *
+
+'''
+A function called count_words that takes a string as an argument
+Returns the amount of words in the string
+'''
+def test_count_words_one_word():
+    assert count_words("test") == 1
