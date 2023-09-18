84865996074720d4253722cc517075b60dce9297
TobyJWalker <tobyjwalker03@outlook.com>
2023-09-18 14:13:33 +0100

    Added errors to pass tests on reading_time

--- lib/reading_time.py
+++ lib/reading_time.py
@@ -1,4 +1,8 @@
 def estimate_reading_time(text):
+
+    if type(text) != str:
+        raise TypeError("Argument must be a string")
+    
     reading_speed = 200 # words per minute
     word_list = text.split()
     return len(word_list) / reading_speed
