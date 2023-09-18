f159c41bdc3e5ffcbdd7fc3cd3832e92c837b5da
TobyJWalker <tobyjwalker03@outlook.com>
2023-09-18 12:10:39 +0100

    Added two word test for make_snippet

--- tests/test_exercise_1.py
+++ tests/test_exercise_1.py
@@ -2,3 +2,6 @@ from lib.exercise_1 import *
 
 def test_make_snippet_one_word():
     assert make_snippet("test") == "test"
+
+def test_make_snippet_two_words():
+    assert make_snippet("test test") == "test test"
